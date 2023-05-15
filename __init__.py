f271fcb9f64dc45670337d7325fb164a7a68e34e
Jason Oleana <jason@geniusvoice.ai>
2023-05-15 10:38:18 +0200

    Update: split in smaller functions and fix errors in code

--- __init__.py
+++ __init__.py
@@ -1,143 +1,105 @@
-# The MIT License (MIT)
-# Copyright (c) 2019 jamesmf
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 from mycroft.util.parse import match_one
 import requests
 
-
-class ButtonValidator:
-    def __init__(self, values, threshold):
-        self.values = values
-        self.threshold = threshold
-
-    def validate(self, utterance):
-        best = match_one(utterance, self.values)
-        return best[1] > self.threshold
-
-
 class RasaSkill(MycroftSkill):
-    button_match_threshold = 0.6
-    button_attempts_max = 3
+    """
+    Mycroft skill that acts as an interface between a Rasa chatbot and a user,
+    allowing continuous voice dialog between the two.
+    """
 
-    # The constructor of the skill, which calls MycroftSkill's constructor
-    def __init__(self):
-        super(RasaSkill, self).__init__(name="RasaSkill")
+    def initialize(self):
+        """
+        Initialize the RasaSkill by setting the Rasa REST endpoint and setting up variables.
+        """
         self.conversation_active = False
-        self.rasa_host = "http://localhost:5005/"
-        self.append_endpoint = (
-            self.rasa_host + "conversations/mycroft_user/messages?include_events=NONE"
-        )
-        self.predict_endpoint = self.rasa_host + "conversations/mycroft_user/predict"
-        self.action_endpoint = (
-            self.rasa_host + "conversations/mycroft_user/execute?include_events=APPLIED"
+        self.convoID = 1
+        self.RASA_API = "http://localhost:5005/webhooks/rest/webhook"
+        self.messages = []
+
+    def send_message_to_rasa(self, msg) -> list:
+        """
+        Send a message to Rasa REST endpoint.
+
+        :param msg: The message to send to Rasa.
+        :return: The response from the Rasa REST endpoint.
+        """
+        data = requests.post(
+            self.RASA_API,
+            json={
+                "message": msg,
+                "sender_id": "user{}".format(self.convoID)
+            }
         )
 
-    @intent_handler(IntentBuilder("").require("Chatwithrasa"))
-    def handle_talk_to_rasa_intent(self, message):
-        response = self.get_response("connecting.to.rasa")
+        return data
+
+    def update_messages(self, data) -> None:
+        """
+        Update the messages list with the responses from Rasa.
+
+        :param data: The response data from the Rasa REST endpoint.
+        """
+        for next_response in data.json():
+            if "text" in next_response:
+                self.messages.append(next_response["text"])
+
+        if len(self.messages) == 0:
+            self.messages = ["no response from rasa"]
+
+    def query_rasa(self, prompt=None):
+        """
+        Query the Rasa chatbot with a given prompt.
+
+        :param prompt: The prompt to send to the Rasa chatbot.
+        :return: The response from the chatbot.
+        """
+        if self.conversation_active == False:
+            return
+
+        if prompt is None and len(self.messages) > 0:
+            prompt = self.messages[-1]
+
+        msg = self.get_response(prompt, num_retries=0)
+        if msg is None:
+            return
+            
+        # flush messages
+        self.messages = []
+        # get rasa response
+        data = self.send_message_to_rasa(msg)
+        # update the messages list
+        self.update_messages(data)
+        # join the messages list
+        prompt = " ".join(self.messages)
+
+        return self.query_rasa(prompt)
+
+    @intent_handler(IntentBuilder("StartChat").require("Chatwithrasa"))
+    def handle_talk_to_rasa_intent(self, message) -> None:
+        """
+        Handle the intent to start a chat with the Rasa chatbot.
+        """
+        self.convoID += 1
         self.conversation_active = True
-        while response is not None and self.conversation_active:
-            messages = self.get_rasa_response(response)
-            if len(messages) > 1:
-                for rasa_message in messages[:-1]:
-                    self.speak(rasa_message)
-            if len(messages) == 0:
-                messages = ["no response from rasa"]
-            response = self.handle_final_output(messages[-1])
+        prompt = "hallo"
+        self.query_rasa(prompt)
 
-        self.speak("disconnecting from rasa")
+    @intent_handler(IntentBuilder("ResumeChat").require("Resume"))
+    def handle_resume_chat(self, message) -> None:
+        """
+        Handle the intent to resume a chat with the Rasa chatbot.
+        """
+        self.conversation_active = True
+        self.query_rasa()
 
-    def handle_final_output(self, message, attempts=0):
-        if attempts > self.button_attempts_max:
-            return None
-        if attempts > 0:
-            self.speak("You can also say Option 1, Option 2, etc")
-        # if we have buttons, handle them
-        if "buttons" in message:
-            # speak the text on the message
-            self.speak(message["text"])
-
-            buttons = message["buttons"]
-            button_titles = [b["title"] for b in buttons]
-
-            if len(buttons) == 1:
-                # if we have a single button, assume it's a confirmation
-                self.speak("To confirm, say")
-            elif len(buttons) > 1:
-                # if we have many buttons, list the options
-                self.speak("You can say")
-            # read out our button title options, separated by "Or"
-            for button in buttons[:-1]:
-                self.speak(button["title"])
-                self.speak("Or")
-            # read the final button option, and await a response
-            # that is in the list of what we just returned OR
-            # is "option 1", "option 2", ..., "option N"  N
-            option_list = [f"option {i+1}" for i in range(len(buttons))]
-            validation_options = button_titles + option_list
-            button_validator = ButtonValidator(
-                validation_options, self.button_match_threshold
-            )
-            print(f"trying attempt {attempts}")
-            response = self.get_response(
-                button_titles[-1],
-                validator=button_validator.validate,
-                num_retries=0,
-                on_fail=self.on_failed_button,
-            )
-            print(f"response {response}")
-            # if response is not None, we passed the validator
-            if response is not None:
-                best_match = match_one(response, validation_options)
-                response = best_match[0]
-                best_ind = validation_options.index(response)
-                if best_ind >= len(buttons):
-                    best_ind = best_ind % len(buttons)
-                response = buttons[best_ind]["payload"]
-                return response
-            return self.handle_final_output(message, attempts=attempts + 1)
-        # if we don't have buttons, just get_response
-        return self.get_response(message["text"], num_retries=0)
-
-    def on_failed_button(self, utt):
-        return "Sorry I didn't catch that."
-
-    def get_rasa_response(self, utterance):
-        if "stop" in utterance.lower():
-            self.conversation_active = False
-            return [{"text": "goodbye from rasa"}]
-        messages = self.hit_rasa(utterance)
-        print(messages)
-        return messages
-
-    def stop(self):
+    def stop(self) -> None:
+        """
+        Stop the current conversation with the Rasa chatbot.
+        """
         self.conversation_active = False
-
-    def hit_rasa(self, utterance):
-        # send our utterance to rasa
-        print(f"sending {utterance} to {self.append_endpoint}")
-        append_response = requests.post(
-            self.append_endpoint, json={"text": utterance, "sender": "user"}
-        )
-        # run actions until we get an action_listen response
-        action = None
-        outputs = []
-        while action != "action_listen":
-            response = requests.post(self.predict_endpoint).json()
-            # send the most likely action to the execute endpoint
-            action = response["scores"][0]["action"]
-            self.log.debug(action)
-            print(f"sending {action} to {self.action_endpoint}")
-            run_action = requests.post(
-                self.action_endpoint, json={"name": action}
-            ).json()
-            outputs += [m for m in run_action["messages"]]
-        self.log.debug(outputs)
-        return outputs
-
 
 def create_skill():
     return RasaSkill()
